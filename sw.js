598d43c72553d6e4a6b57a3e1cb7596a1937c955
Barla Fam <nd4zmoe@gmail.com>
2026-04-21 08:58:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Network-First (Anti-Captive Portal), & Aggressive Memory Trimmer.
  */
 
-const APP_VERSION = '30.9-OMEGA'; 
+const APP_VERSION = '40.0-OMEGA'; 
 
 const CACHE_CORE = 'fambarla-core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'fambarla-dynamic-v' + APP_VERSION;
