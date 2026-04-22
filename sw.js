938757874ec5c472ff825b41ec5603b8745f2b99
Barla Fam <nd4zmoe@gmail.com>
2026-04-22 09:05:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Architecture: Clean Cache-Key, Strict Install, Network-First, & Aggressive Memory Trimmer.
  */
 
-const APP_VERSION = '1.4'; 
+const APP_VERSION = '1.0'; 
 
 const CACHE_CORE = 'fambarla-core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'fambarla-dynamic-v' + APP_VERSION;
